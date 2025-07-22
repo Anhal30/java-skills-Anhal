c95fe882c5c847ffc10e69252c7db07fa11b1614
Anhal Alruziqi <anhal9455@gmail.com>
2025-07-22 17:00:32 +0400

    Add PatternMaker class for loop patterns

---
+++ src/PatternMaker.java
@@ -0,0 +1,17 @@
+public class PatternMaker {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            // Spaces
+            for (int j = i; j < rows; j++) {
+                System.out.print(" ");
+            }
+            // Stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
